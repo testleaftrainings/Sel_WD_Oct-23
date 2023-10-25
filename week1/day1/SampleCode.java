feec7c7ba249889d2edeb5c9784e0d7cffc73b79
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2023-10-25 16:51:33 +0530

    Add files via upload

---
+++ week1/day1/SampleCode.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class SampleCode {
+	
+	
+	/*
+	 * To use single line command line - do ctrl + /
+	 * To command multiple lines -- do ctrl + shift + /
+	 * 
+	 * Execution - right click -> run as -> Java appliaction
+	 * 			- Ctrl + f11
+	 * 
+	 * Main method is the entry point to execute the program Shortcut to write main
+	 * method --> type main & do ctrl + spacebar & press enter key
+	 */
+	public static void main(String[] args) {
+		// All the logic will be written inside the method.
+		
+		System.out.print("Hello world");
+		
+		// syso --> ctrl + spacebar & enter
+		System.out.println("Welcome to Testleaf");
+		
+		
+	}
+
+}
